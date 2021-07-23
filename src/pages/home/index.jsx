4a35f11b55c4308a5067ee2e01c88fc88f1597d4
Matheus <matheusrdeoliv1@gmail.com>
2021-07-23 19:40:29 -0300

    implementando login page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import MainLogo from '../../assets/ifuture-logo-white.svg'
 import { useHistory } from "react-router-dom"
+import DisplayFlexCenter from '../../themes/flexEffect'
 
 const Body = styled.body`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  ${DisplayFlexCenter};
   height: 100vh;
   background-color: #e8222e;
 `
